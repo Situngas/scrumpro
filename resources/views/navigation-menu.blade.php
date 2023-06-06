021e0179a945ed2f953b95969e54742152f2632e
Situngas <114785473+Situngas@users.noreply.github.com>
2023-06-06 13:54:57 -0600

    new file:   public/assets/css/styles.css 	new file:   public/assets/favicon.ico 	new file:   public/assets/img/bg-masthead.jpg 	new file:   public/assets/img/bg-signup.jpg 	new file:   public/assets/img/demo-image-01.jpg 	new file:   public/assets/img/demo-image-02.jpg 	new file:   public/assets/img/ipad.png 	new file:   public/assets/js/scripts.js 	modified:   resources/views/components/application-mark.blade.php 	modified:   resources/views/components/authentication-card-logo.blade.php 	modified:   resources/views/dashboard.blade.php 	modified:   resources/views/navigation-menu.blade.php 	modified:   resources/views/welcome.blade.php

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -15,6 +15,15 @@
                     <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Alumnos') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Notas') }}
+                    </x-nav-link>
+                    <x-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Horas de Servicio') }}
+                    </x-nav-link>
                 </div>
             </div>
 
